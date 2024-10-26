389c6f7caddbda82bd0c26088bb3e6c95bd7ad2e
kazumov <lesha.kazymoff@yandex.ru>
2024-10-27 03:27:21 +0500

    Добавлены пакеты и разработан класс config для подключения к базе данных

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Films_kazumov.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;port=3307;uid=root;pwd=;database=FilmsAndGenres;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
