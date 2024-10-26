094a658647de24ee4ed65065cd50b813709a943d
kazumov <lesha.kazymoff@yandex.ru>
2024-10-27 04:09:35 +0500

    Реализован контекст фильмов

---
+++ Context/FilmsContext.cs
@@ -0,0 +1,22 @@
+﻿using Films_kazumov.Classes.Database;
+using Films_kazumov.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films_kazumov.Context
+{
+    public class FilmsContext : DbContext
+    {
+        public DbSet<Films> Films { get; set; }
+        public FilmsContext()
+        {
+            Database.EnsureCreated();
+            Films.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+    }
+}
