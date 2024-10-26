6e6d06ebd9b5426ae5a8b44206bdd2af359ada8a
kazumov <lesha.kazymoff@yandex.ru>
2024-10-27 04:04:08 +0500

    Реализовано модель фильмов

---
+++ Models/Films.cs
@@ -0,0 +1,111 @@
+﻿using Films_kazumov.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace Films_kazumov.Models
+{
+    public class Films : Notification
+    {
+        public int Id { get; set; }
+        public string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,50}$");
+                if (!match.Success)
+                    MessageBox.Show("Наименование не должно быть пустым, и не более 50 символов.", "Не корректный ввод значения.");
+                else
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+
+            }
+        }
+        private string price;
+        public string Price
+        {
+            get { return price; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,30}$");
+                if (!match.Success)
+                    MessageBox.Show("Цена не должна быть пустая, и не более 30 символов.", "Не корректный ввод значения.");
+                else
+                {
+                    price = value;
+                    OnPropertyChanged("Price");
+                }
+            }
+        }
+        private DateTime dateStart;
+        public DateTime DateStart
+        {
+            get { return dateStart; }
+            set
+            {
+                dateStart = value;
+                OnPropertyChanged("DateStart");
+            }
+        }
+
+        [Schema.NotMapped]
+        public bool isEnable;
+        [Schema.NotMapped]
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+                OnPropertyChanged("IsEnableText");
+            }
+        }
+        [Schema.NotMapped]
+        public string IsEnableText
+        {
+            get
+            {
+                if (IsEnable) return "Сохранить";
+                else return "Изменить";
+            }
+        }
+        
+        [Schema.NotMapped]
+        public RealyCommand OnEdit
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    IsEnable = !IsEnable;
+                    if (!IsEnable)
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_films.filmsContext.SaveChanges();
+                });
+            }
+        }
+        [Schema.NotMapped]
+        public RealyCommand OnDelete
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    if (MessageBox.Show("Вы уверены что хотите удалить фильм?", "Предупреждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_films.Films.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_films.filmsContext.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_films.filmsContext.SaveChanges();
+                    }
+                });
+            }
+        } 
+    }
+}
---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Films_kazumov.ViewModels
+{
+    public class VM_Pages
+    {
+    }
+}
