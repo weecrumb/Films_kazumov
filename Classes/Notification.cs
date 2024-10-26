1c3acec940fa925dd37a3065662b18732466984f
kazumov <lesha.kazymoff@yandex.ru>
2024-10-27 03:32:02 +0500

    Разработаны классы для работы с программой и бд

---
+++ Classes/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Films_kazumov.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Classes/RealyCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Films_kazumov.Classes
+{
+    public class RealyCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        public RealyCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+        public void Execute(object parameter) =>
+            this.execute(parameter);
+    }
+}
